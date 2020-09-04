8c0550d5fdde8b25d189a6d90986b0a6a7e8b95d
CMSTie <cmstie@gmail.com>
2020-09-04 13:43:25 -0400

    cleaned up code

--- app/serializers/user_tracked_object_serializer.rb
+++ app/serializers/user_tracked_object_serializer.rb
@@ -1,5 +1,3 @@
 class UserTrackedObjectSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :near_earth_object_id, :exoplanet_id, :comment, :user, :near_earth_object, :exoplanet
-  # belongs_to :user
-  # belongs_to :near_earth_object
 end
